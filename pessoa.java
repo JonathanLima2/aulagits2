6276187af96ec3fe5826b44b4d1ea3844bf5eb59
Jonathan Lima <jdsl@icomp.ufam.edu.br>
2022-05-12 21:37:30 -0400

    atualização da função pular em while

--- pessoa.java
+++ pessoa.java
@@ -41,9 +41,13 @@ class jsl extends pessoa{
     public int getP(){return this.pulacorda;}
 
     public int pular(int i)
-    {
-        for (int n=0; n<=i; n++)
-            this.setP(n);
+    { 
+        int k=0;     
+        while(k<=i)
+        { 
+            this.setP(k);
+            k++;
+        }
         return this.getP();
     }
 }
